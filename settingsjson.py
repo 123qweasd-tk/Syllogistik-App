9c08334f221527a62f4fb1da272882f16839ffa6
123qweasd-tk <71277656+123qweasd-tk@users.noreply.github.com>
2024-06-27 16:29:36 +0200

    Update settingsjson.py
    rechnen wechselbar in einstellungen

--- settingsjson.py
+++ settingsjson.py
@@ -2,6 +2,11 @@ import json
 
 settings_json = json.dumps([
     {'type': 'bool',
+     'title': 'Wechsle zwischen Üben und Ausrechnen',
+     'desc': 'Entweder das Programm rechnet für dich oder dir werden die Prämissen als Geltungswertformeln angezeigt',
+     'section': 'trainer',
+     'key': 'calculate_exercise'},
+    {'type': 'bool',
      'title': 'Wechsle zu vollständiger Syllogistik',
      'desc': 'Vollständige Syllogistik beinhaltet auch ã-, ë-, ï- and õ-Urteile',
      'section': 'trainer',
